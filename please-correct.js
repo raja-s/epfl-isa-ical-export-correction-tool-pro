75cdc419321b93a15e8d02e5b585334d18518b11
raja-s <rjsoufi@gmail.com>
2018-04-24 13:17:14 +0200

    Added constant strings and traversal states

--- please-correct.js
+++ please-correct.js
@@ -8,21 +8,70 @@
 const ARGUMENT_COUNT = process.argv.length - 2;
 
 // Valid correction languages
-const LANGUAGES = Object.freeze({
+const LANGUAGES = {
     ENGLISH : 'en',
     FRENCH  : 'fr'
-});
+};
 
 // Default correction language
 const DEFAULT_LANGUAGE = LANGUAGES.FRENCH;
 
+// Event categories
+const CATEGORIES = {
+    LECTURE  : 'Cours',
+    EXERCISE : 'Exercices',
+    PROJECT  : 'Projet',
+    LAB      : 'Labo' // TODO: Check
+};
+
+// Strings to look for during the traversal
+const MATCHES = {
+    BEGIN_EVENT : 'BEGIN:VEVENT',
+    END_EVENT   : 'END:VEVENT',
+    SUMMARY     : 'SUMMARY:',
+    CATEGORIES  : 'CATEGORIES:'
+};
+
+// Event category-title mappings for each language
+const LANGUAGE_MAPPINGS = {};
+
+LANGUAGE_MAPPINGS[LANGUAGES.FRENCH] = {};
+
+LANGUAGE_MAPPINGS[LANGUAGES.FRENCH][CATEGORIES.LECTURE]  = CATEGORIES.LECTURE;
+LANGUAGE_MAPPINGS[LANGUAGES.FRENCH][CATEGORIES.EXERCISE] = CATEGORIES.EXERCISE;
+LANGUAGE_MAPPINGS[LANGUAGES.FRENCH][CATEGORIES.PROJECT]  = CATEGORIES.PROJECT;
+LANGUAGE_MAPPINGS[LANGUAGES.FRENCH][CATEGORIES.LAB]      = CATEGORIES.LAB;
+
+LANGUAGE_MAPPINGS[LANGUAGES.ENGLISH] = {};
+
+LANGUAGE_MAPPINGS[LANGUAGES.ENGLISH][CATEGORIES.LECTURE]  = 'Lecture';
+LANGUAGE_MAPPINGS[LANGUAGES.ENGLISH][CATEGORIES.EXERCISE] = 'Exercise';
+LANGUAGE_MAPPINGS[LANGUAGES.ENGLISH][CATEGORIES.PROJECT]  = 'Project';
+LANGUAGE_MAPPINGS[LANGUAGES.ENGLISH][CATEGORIES.LAB]      = 'Lab';
+
+// Traversal states
+const STATES = {
+    
+    // Seeking the 'BEGIN' tag of the next event (initial state)
+    SEEKING_NEXT_EVENT     : 0,
+    
+    // Event found, seeking the event's 'CATEGORIES' tag
+    // whose value is one of the known categories
+    SEEKING_EVENT_CATEGORY : 1,
+    
+    // Event category identified, seeking the event's 'SUMMARY'
+    // tag in order to insert the category string in its value
+    SEEKING_EVENT_SUMMARY  : 2
+    
+};
+
 // Error exit codes
-const ERRORS = Object.freeze({
+const ERRORS = {
     INCORRECT_NUMBER_OF_ARGUMENTS : 1,
     UNRECOGNIZED_LANGUAGE         : 2,
     READ_FILE                     : 3,
     WRITE_FILE                    : 4
-});
+};
 
 // CLI usage text
 const USAGE =
